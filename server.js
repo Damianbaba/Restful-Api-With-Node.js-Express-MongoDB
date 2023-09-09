d929f5c1f2db3c3240f35f636fd7b622d79d7297
Damian Mroczek <damianmroczek@yahoo.com>
2023-09-09 17:28:19 +0700

    delete product from DB

--- server.js
+++ server.js
@@ -63,6 +63,22 @@ app.put('/products/:id', async (req, res) => {
     }
 })
 
+// DELETE PRODUCT
+
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ message: `cannot find any product with id ${id}` })
+        }
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
 mongoose
     .connect(
         "mongodb+srv://admin:adminadmin@cluster0.t0a8klp.mongodb.net/Node-API?retryWrites=true&w=majority"
