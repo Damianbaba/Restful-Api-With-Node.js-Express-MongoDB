ea16cf897015f56f11f8160869ca0f5ea54f2697
Damian Mroczek <damianmroczek@yahoo.com>
2023-09-09 15:24:36 +0700

    connecting app to MongoDB

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // routes
 
-app.get('/', (req, res) => {
-    res.send('Hello Node API')
-})
-app.get('/post', (req, res) => {
-    res.send('Hello Node API post')
-})
-
-app.listen(3000, () => {
-    console.log('Node API app is runnung on port 3000');
-
+app.get("/", (req, res) => {
+    res.send("Hello Node API");
 });
+app.get("/blog", (req, res) => {
+    res.send("Hello blog, My name is ...");
+});
+
+mongoose
+    .connect(
+        "mongodb+srv://admin:adminadmin@cluster0.t0a8klp.mongodb.net/Node-API?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        console.log("connected to MongoDB");
+        app.listen(3000, () => {
+            console.log("Node API app is runnung on port 3000");
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
